daec252bb832994b1aaa093790503be195f398c7
woods-ming <woodsming@sina.com>
2018-10-21 21:18:14 +0800

    java简介 基础概念

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,15 @@
-$("img[data-original]").lazyload({
-    threshold : -10
-});
+(function() {
+
+	'use strict';
+	$("img[data-original]").lazyload({
+		threshold : -10
+	});
+
+	$("dl.dl-horizontal").on('click', function(){
+		$('dl.dl-horizontal.focus').removeClass('focus');
+		$(this).addClass('focus');
+	})
+	$('h2, h3').on('click', function(){
+		$('dl.dl-horizontal.focus').removeClass('focus');
+	});
+})();
